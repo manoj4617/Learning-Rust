4ab399d2f2a88b9d2bdcac164a0a44703649f9e9
manoj4617 <manojkumarbagewadi4617@gmail.com>
2022-09-29 21:27:28 +0530

    second cargo project

---
+++ my_second_project/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    println!("This is my second rust projec using cargo");
+    println!("Printing this line");
+    println!("Printing this line again");
+    println!("Printing this to see if the checking takes more time than it took before");
+
+}
